4414c48c299dea3e319338842750b4e4a7dabb0a
ChiSrwi <tonyqisirui@gmail.com>
2023-02-15 23:46:08 -0500

    The bash which was changed for running code.

---
+++ demo-word.sh
@@ -0,0 +1,19 @@
+normalize_text() {
+  awk '{print tolower($0);}' | sed -e "s/’/'/g" -e "s/′/'/g" -e "s/''/ /g" -e "s/'/ ' /g" -e "s/“/\"/g" -e "s/”/\"/g" \
+  -e 's/"/ " /g' -e 's/\./ \. /g' -e 's/<br \/>/ /g' -e 's/, / , /g' -e 's/(/ ( /g' -e 's/)/ ) /g' -e 's/\!/ \! /g' \
+  -e 's/\?/ \? /g' -e 's/\;/ /g' -e 's/\:/ /g' -e 's/-/ - /g' -e 's/=/ /g' -e 's/=/ /g' -e 's/*/ /g' -e 's/|/ /g' \
+  -e 's/«/ /g' | tr 0-9 " "
+}
+make
+if [ ! -e text8 ]; then
+  wget http://mattmahoney.net/dc/text8.zip -O text8.gz
+  gzip -d text8.gz -f
+fi
+if [ ! -e data.txt ]; then
+  for i in `ls 1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled`; do
+    echo 'Time: ' $i
+    normalize_text < 1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled/$i >> data.txt
+  done
+fi
+time ./word2vec -train data.txt -output vectors.bin -cbow 1 -size 500 -window 10 -negative 10 -hs 0 -sample 1e-5 -threads 40 -binary 1 -iter 3 -min-count 10
+./distance vectors.bin
